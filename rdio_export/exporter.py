12717156c6aeffd173ca773b759887ccddddb22c
Tristan Waddington <tristan.waddington@gmail.com>
2015-05-30 15:50:10 -0700

    Revert "Merge pull request #7 from jtan189/master"
    This reverts commit 8f38dcfa984b771ccc134a023114009d1d2080fc, reversing
    changes made to 1f3c950daf513603af3431f38baaf7e47430aaf3.

--- rdio_export/exporter.py
+++ rdio_export/exporter.py
@@ -13,7 +13,6 @@ RDIO_C_EXTRAS = ','.join((
         'artist',
         'displayDate',
         'shortUrl',
-        'album',
         'albumKey',
         'artistKey',
 ))
@@ -76,7 +75,7 @@ class RdioExporter():
         # Continue fetching in batches until the last response is smaller
         # than the page size (count).
         while (resp_size == count):
-            collection = self._call('collection', 'getTracksInCollection',
+            collection = self._call('collection', 'getAlbumsInCollection',
                     user=user.get('key'), sort=sort, start=start, count=count, extras=extras)
 
             # Update the size of the last response
